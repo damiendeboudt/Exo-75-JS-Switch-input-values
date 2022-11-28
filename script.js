54f1bf78e9853b93603d91302f9a43486b00d44b
Damien <damien.deboudt@sfr.fr>
2022-11-28 10:33:30 +0100

    ok

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let switcher = document.getElementById("switch");
 
 function switchy () {
-    let input12 = document.getElementById("input1");
-    let input23 = document.getElementById("input2");
-    input12.value = input23.value
+    let box1 = document.getElementById("input1");
+    let box2 =document.getElementById("input2");
+    let value1 = document.getElementById("input1").value;
+    let value2 = document.getElementById("input2").value;
+    box1.value = value2
+    box2.value = value1
+
 }
 switcher.addEventListener("click", switchy)
 
